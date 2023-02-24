19ccf4dec7b0ad7d8f0d2ee836511bb46ca48e43
Spike Owen <sowen3373@gmail.com>
2023-02-24 01:54:56 +0000

    Boids with Separation added

--- FYPBoids/Assets/Scripts/BoidBehaviour.cs
+++ FYPBoids/Assets/Scripts/BoidBehaviour.cs
@@ -4,25 +4,57 @@ using UnityEngine;
 
 public class BoidBehaviour : MonoBehaviour
 {
-    Vector3 position;
-    GameObject gameObject;
+    public int SwarmIndex { get; set; }
+    public float NoClumpingRadius { get; set; }
+    public float LocalAreaRadius { get; set; }
+    public float Speed { get; set; }
+    public float SteeringSpeed { get; set; }
 
-    float speed;
-    Vector3 direction;
-    public List<BoidBehaviour> boidsInScene;
-
-    // Start is called before the first frame update
-    void Start()
+    public void SimulateMovement(List<BoidBehaviour> other, float time)
     {
-        
-    }
+        //Steering vars
+        var steering = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
-    {
-        position = transform.position;
-        transform.Translate(direction * (speed * Time.deltaTime));
+        //Separation vars and process
+        Vector3 separationDirection = Vector3.zero;
+        int separationCount = 0;
+        foreach (BoidBehaviour boid in other)
+        {
+            //skip self
+            if (boid == this)
+                continue;
+
+            var distance = Vector3.Distance(boid.transform.position, this.transform.position);
+
+            //if another boid is within close proximity
+            if (distance < NoClumpingRadius)
+            {
+                separationDirection += boid.transform.position - transform.position;
+                separationCount++;
+            }
+        }
+
+        //Calculate average position dependant on the boid(s) that are too close
+        //Because if there's more than one, the direction will be too high, getting the average gives the direction that is away from ALL of them
+        if (separationCount > 0)
+            separationDirection /= separationCount;
+
+        //flip and normalize
+        separationDirection = -separationDirection.normalized;
+
+        //apply direction to steering
+        steering = separationDirection;
+
+        //apply steering
+        if (steering != Vector3.zero)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(steering), SteeringSpeed * time);
+
+        //move 
+        transform.position += transform.TransformDirection(new Vector3(0, 0, Speed)) * time;
     }
 }
 
-//https://blog.yarsalabs.com/flock-simulation-using-boids-in-unity/#:~:text=Boid%20simulation%20is%20the%20simulation,controlled%20by%20a%20single%20creature. Research Link
+
+// Research Links in case
+//https://blog.yarsalabs.com/flock-simulation-using-boids-in-unity/#:~:text=Boid%20simulation%20is%20the%20simulation,controlled%20by%20a%20single%20creature.
+//https://dawn-studio.de/tutorials/boids/
---
+++ FYPBoids/Assets/Scripts/SceneController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public BoidBehaviour BoidPrefab;
+
+    public int spawnBoids = 100;
+    public float boidSpeed = 10f;
+    public float boidSteeringSpeed = 100f;
+
+    //How close before steering away from others/Swarm density
+    public float boidNoClumpingArea = 10f;
+    //Range of influence/Swarm Size
+    public float boidLocalArea = 10f;
+
+    //Boid viewing area
+    public float boidSimulationArea = 50f;
+
+    private List<BoidBehaviour> boidList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boidList = new List<BoidBehaviour>();
+
+        for (int i = 0; i < spawnBoids; i++)
+        {
+            SpawnBoid(BoidPrefab.gameObject, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (BoidBehaviour boid in boidList)
+        {
+            boid.SimulateMovement(boidList, Time.deltaTime);
+
+            //Keeps boids within set area, and loops them to the other side like pacman
+            var boidPos = boid.transform.position;
+
+            if (boidPos.x > boidSimulationArea)
+                boidPos.x -= boidSimulationArea * 2;
+            else if (boidPos.x < -boidSimulationArea)
+                boidPos.x += boidSimulationArea * 2;
+
+            if (boidPos.y > boidSimulationArea)
+                boidPos.y -= boidSimulationArea * 2;
+            else if (boidPos.y < -boidSimulationArea)
+                boidPos.y += boidSimulationArea * 2;
+
+            if (boidPos.z > boidSimulationArea)
+                boidPos.z -= boidSimulationArea * 2;
+            else if (boidPos.z < -boidSimulationArea)
+                boidPos.z += boidSimulationArea * 2;
+
+            boid.transform.position = boidPos;
+        }
+    }
+
+    private void SpawnBoid(GameObject prefab, int swarmIndex)
+    {
+        var boidInstance = Instantiate(prefab);
+        boidInstance.transform.localPosition += new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+
+        var boidBehaviour = boidInstance.GetComponent<BoidBehaviour>();
+        //Swarm index is used to track what swarm a boid belongs to if there are multiple
+        boidBehaviour.SwarmIndex = swarmIndex;
+        boidBehaviour.Speed = boidSpeed;
+        boidBehaviour.SteeringSpeed = boidSteeringSpeed;
+        boidBehaviour.LocalAreaRadius = boidLocalArea;
+        boidBehaviour.NoClumpingRadius = boidNoClumpingArea;
+
+        boidList.Add(boidBehaviour);
+    }
+}
