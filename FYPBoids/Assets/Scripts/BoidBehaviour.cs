156147e408f20df330634bac9d00ef9008b585e1
Spike Owen <72083521+spikeowen@users.noreply.github.com>
2023-02-24 16:45:59 +0000

    Added Alignment+cohesion, with ability to change weight in-engine

--- FYPBoids/Assets/Scripts/BoidBehaviour.cs
+++ FYPBoids/Assets/Scripts/BoidBehaviour.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class BoidBehaviour : MonoBehaviour
 {
+    [SerializeField]
     public int SwarmIndex { get; set; }
     public float NoClumpingRadius { get; set; }
     public float LocalAreaRadius { get; set; }
     public float Speed { get; set; }
     public float SteeringSpeed { get; set; }
+
+    public float SeparationWeight = 0.5f;
+    public float AlignmentWeight = 0.25f;
+    public float CohesionWeight = 0.25f;
 
     public void SimulateMovement(List<BoidBehaviour> other, float time)
     {
@@ -18,6 +23,15 @@ public class BoidBehaviour : MonoBehaviour
         //Separation vars and process
         Vector3 separationDirection = Vector3.zero;
         int separationCount = 0;
+
+        //Alignment vars and process
+        Vector3 alignmentDirection = Vector3.zero;
+        int alignmentCount = 0;
+
+        //Separation vars and process
+        Vector3 cohesionDirection = Vector3.zero;
+        int cohesionCount = 0;
+
         foreach (BoidBehaviour boid in other)
         {
             //skip self
@@ -26,11 +40,21 @@ public class BoidBehaviour : MonoBehaviour
 
             var distance = Vector3.Distance(boid.transform.position, this.transform.position);
 
-            //if another boid is within close proximity
+            //if another boid is within close proximity - SEPARATION
             if (distance < NoClumpingRadius)
             {
                 separationDirection += boid.transform.position - transform.position;
                 separationCount++;
+            }
+
+            //if another boid is generally nearby - ALIGNMENT + COHESION
+            if (distance < LocalAreaRadius)
+            {
+                alignmentDirection += boid.transform.forward;
+                alignmentCount++;
+
+                cohesionDirection += boid.transform.position - transform.position;
+                cohesionCount++;
             }
         }
 
@@ -39,11 +63,31 @@ public class BoidBehaviour : MonoBehaviour
         if (separationCount > 0)
             separationDirection /= separationCount;
 
-        //flip and normalize
+        //flip and normalize direction
         separationDirection = -separationDirection.normalized;
 
-        //apply direction to steering
-        steering = separationDirection;
+        //Calculate average position dependant on the boid(s) that are too close
+        if (alignmentCount > 0)
+            alignmentDirection /= alignmentCount;
+
+        //Calculate average position dependant on the boid(s) that are too close
+        if (cohesionCount > 0)
+            cohesionDirection /= cohesionCount;
+
+        //Keep direction relative to boid's center
+        cohesionDirection -= transform.position;
+
+        ////apply separation direction to steering
+        //steering = separationDirection;
+        ////apply alignment direction to steering
+        //steering += alignmentDirection;
+        ////apply cohesion direction to steering
+        //steering += cohesionDirection;
+
+
+        steering += separationDirection.normalized * SeparationWeight;
+        steering += alignmentDirection.normalized * AlignmentWeight;
+        steering += cohesionDirection.normalized * CohesionWeight;
 
         //apply steering
         if (steering != Vector3.zero)
