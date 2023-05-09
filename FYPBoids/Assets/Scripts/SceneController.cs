4bacd379064051af03d18f92b889c3f5360c03bd
Spike Owen <sowen3373@gmail.com>
2023-05-09 14:17:43 +0100

    Testing, need tournament results

--- FYPBoids/Assets/Scripts/SceneController.cs
+++ FYPBoids/Assets/Scripts/SceneController.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-//Current STATUS: Program runs, boids don't reactivate and yet new gens have diff swarm scores
-//Need to pass boids back to overwrite and reactivate boids in scene
 
 public class SceneController : MonoBehaviour
 {
